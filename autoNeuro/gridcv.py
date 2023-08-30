175a6b539d58b07ebbc2cfb02ec98f6175e01d7f
maryjis <m.zubrikhina2014@yandex.ru>
2023-08-30 11:18:05 +0300

    Change feature reduction parameters

--- autoNeuro/gridcv.py
+++ autoNeuro/gridcv.py
@@ -185,10 +185,10 @@ class GridSearchBase:
 
             if feature_reductions:
                 self.n_features = [round(self.X.shape[0] * 0.3), round(self.X.shape[0] * 0.1),3]
-                self.feature_selection_methods +=[Isomap(n_components=n,n_jobs=-1) for n in self.n_features]
-                self.feature_selection_methods +=[LocallyLinearEmbedding(n_components=n,method='modified', n_neighbors=n+5,
-                                                                         n_jobs=-1)
-                                              for n in self.n_features]
+                # self.feature_selection_methods +=[Isomap(n_components=n,n_jobs=-1) for n in self.n_features]
+                # self.feature_selection_methods +=[LocallyLinearEmbedding(n_components=n,method='modified', n_neighbors=n+5,
+                #                                                          n_jobs=-1)
+                #                               for n in self.n_features]
                 self.feature_selection_methods += [PCA(n, random_state=self.random_state) for n in self.n_features]
             else:
                 self.n_features = [round(self.X.shape[0]), round(self.X.shape[0] * 0.8)]
